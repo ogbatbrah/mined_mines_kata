a971c65dd28bbb6d8700f7b786d114cf05203414
jacob <jakechap08@gmail.com>
2018-06-06 14:54:17 -0400

    added 15s to the mix

--- print_2_screen.rb
+++ print_2_screen.rb
@@ -18,16 +18,16 @@
 #  number = number + 1
 # end
 
-number = 1
-100.times do
-	if number %15 == 0
-		puts 'minedminds'
- elsif number %3 == 0
-    puts 'mined'
- elsif number %5 == 0
-   puts 'minds'
- else
-   puts number
- end
- number = number + 1
-end
+# number = 1
+# 100.times do
+# 	if number %15 == 0
+# 		puts 'minedminds'
+#  elsif number %3 == 0
+#     puts 'mined'
+#  elsif number %5 == 0
+#    puts 'minds'
+#  else
+#    puts number
+#  end
+#  number = number + 1
+# end
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Day 2 mined minds
+Day 2 exercises
