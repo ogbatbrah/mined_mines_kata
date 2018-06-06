f4619158c6e7534ffc78cfbb5ddeb73de6d42c5e
jacob <jakechap08@gmail.com>
2018-06-06 14:00:12 -0400

    change 3s to mined and 5s to mines

---
+++ print_2_screen.rb
@@ -0,0 +1,18 @@
+# p 1
+# puts 1
+# print 1
+#  100.times do
+#  puts 1
+#  end
+
+number = 1
+100.times do
+ if number %3 == 0
+   puts 'mined'
+ elsif number %5 == 0
+   puts 'minds'
+ else
+   puts number
+ end
+ number = number + 1
+end
